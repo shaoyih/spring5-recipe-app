5368a7277c4aefbee62ea0661f91ed8b6d8e3e92
Shaoyi <shaoyih2@gmail.com>
2019-07-21 13:37:27 -0700

    Updated function "create a recipe"

--- src/main/java/guru/springframework/Controllers/RecipeController.java
+++ src/main/java/guru/springframework/Controllers/RecipeController.java
@@ -2,9 +2,12 @@ package guru.springframework.Controllers;
 
 
 import guru.springframework.Services.RecipeService;
+import guru.springframework.commands.RecipeCommand;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,5 +21,17 @@ public class RecipeController {
     public String showById(@PathVariable String id, Model model){
         model.addAttribute("recipe",recipeService.findById(new Long(id)));
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe",new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    @PostMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand){
+        RecipeCommand savedRecipeCommand= recipeService.saveRecipeCommand(recipeCommand);
+        return "redirect:/recipe/show/"+savedRecipeCommand.getId();
     }
 }
--- src/main/java/guru/springframework/Converters/UnitOfMeasureCommandToUnitOfMeasure.java
+++ src/main/java/guru/springframework/Converters/UnitOfMeasureCommandToUnitOfMeasure.java
@@ -5,7 +5,9 @@ import guru.springframework.domain.UnitOfMeasure;
 import lombok.Synchronized;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
 
+@Component
 public class UnitOfMeasureCommandToUnitOfMeasure implements Converter<UnitOfMeasureCommand, UnitOfMeasure> {
     @Synchronized
     @Nullable
